53456c7628c4068b9ee1325cbd06f58549375fd2
duca <jovanovdusan1@gmail.com>
2016-12-27 20:48:57 +0100

    promenio naziv klase

--- app/src/main/java/com/dusanjovanov/meetups3/StandardRequest.java
+++ app/src/main/java/com/dusanjovanov/meetups3/StandardRequest.java
@@ -18,30 +18,36 @@ import java.util.Map;
  * Created by duca on 26/12/2016.
  */
 
-public class GsonRequest<T> extends Request<T> {
+public class StandardRequest<T> extends Request<T> {
 
+    private static final String BASE_URL = "http://192.168.1.7/api/v1";
     private final Gson gson = new Gson();
     private final Class<T> clazz;
-    private final Map<String, String> headers = new HashMap<>();
     private final ResponseListener<T> listener;
+    private Map<String, String> headers;
+    private Map<String,String> params;
 
     {
+        headers = new HashMap<>();
+        headers.put("authorization","801bb1e7ebfc127b17fb1330335701c2");
         headers.put("Content-Type","application/x-www-form-urlencoded");
-        headers.put("Authorization","801bb1e7ebfc127b17fb1330335701c2");
     }
 
     public interface ResponseListener<T> {
         void onResponse(T response);
     }
 
-    public GsonRequest(int method,
+    public StandardRequest(int method,
                        String url,
+                       Map<String,String> params,
+                       Class<T> clazz,
                        ResponseListener<T> listener,
-                       Response.ErrorListener errorListener,
-                       Class<T> clazz) {
-        super(method, url, errorListener);
+                       Response.ErrorListener errorListener){
+        super(method,BASE_URL+url,errorListener);
+        this.params = params;
         this.clazz = clazz;
         this.listener = listener;
+
     }
 
     @Override
@@ -52,7 +58,7 @@ public class GsonRequest<T> extends Request<T> {
     //TODO
     @Override
     protected Map<String, String> getParams() throws AuthFailureError {
-        return super.getParams();
+        return params;
     }
 
     @Override
