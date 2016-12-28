fc88aed3f216c85ebde5917948110208296479ae
duca <jovanovdusan1@gmail.com>
2016-12-28 18:29:43 +0100

    dodao model za korisnike

---
+++ app/src/main/java/com/dusanjovanov/meetups3/models/User.java
@@ -0,0 +1,51 @@
+package com.dusanjovanov.meetups3.models;
+
+/**
+ * Created by duca on 28/12/2016.
+ */
+
+public class User {
+    private int id;
+    private String displayName;
+    private String email;
+    private String photoUrl;
+
+    public User(int id, String displayName, String email, String photoUrl) {
+        this.id = id;
+        this.displayName = displayName;
+        this.email = email;
+        this.photoUrl = photoUrl;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public void setDisplayName(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhotoUrl() {
+        return photoUrl;
+    }
+
+    public void setPhotoUrl(String photoUrl) {
+        this.photoUrl = photoUrl;
+    }
+}
