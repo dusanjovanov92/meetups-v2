ec44828c225672632629e1ef6b86d2ed1ab44833
duca <jovanovdusan1@gmail.com>
2016-12-31 14:52:52 +0100

    napravio promene u klasi za rad sa retrofitom , singleton i jos jedan poziv

--- app/src/main/java/com/dusanjovanov/meetups3/rest/ApiClient.java
+++ app/src/main/java/com/dusanjovanov/meetups3/rest/ApiClient.java
@@ -15,37 +15,34 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ApiClient {
     public static final String BASE_URL = "http://192.168.1.7/api/v1/";
-    private static Retrofit retrofit = null;
-    private static OkHttpClient client = null;
 
+    private static final OkHttpClient client = new OkHttpClient.Builder()
+            .addInterceptor(new Interceptor() {
+                @Override
+                public Response intercept(Chain chain) throws IOException {
+                    Request original = chain.request();
 
-    public static Retrofit getRetrofit(){
-        if(client==null){
-            client = new OkHttpClient.Builder()
-                    .addInterceptor(new Interceptor() {
-                        @Override
-                        public Response intercept(Chain chain) throws IOException {
-                            Request original = chain.request();
+                    Request request = original.newBuilder()
+                            .header("authorization", "801bb1e7ebfc127b17fb1330335701c2")
+                            .header("Content-Type", "application/x-www-form-urlencoded")
+                            .method(original.method(), original.body())
+                            .build();
 
-                            Request request = original.newBuilder()
-                                    .header("authorization", "801bb1e7ebfc127b17fb1330335701c2")
-                                    .header("Content-Type", "application/x-www-form-urlencoded")
-                                    .method(original.method(), original.body())
-                                    .build();
+                    return chain.proceed(request);
+                }
+            })
+            .build();
 
-                            return chain.proceed(request);
-                        }
-                    })
-                    .build();
-        }
+    private static final Retrofit retrofit = new Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(client)
+            .build();
 
-        if(retrofit==null){
-            retrofit = new Retrofit.Builder()
-                    .baseUrl(BASE_URL)
-                    .addConverterFactory(GsonConverterFactory.create())
-                    .client(client)
-                    .build();
-        }
-        return retrofit;
+    private static final MeetupsApi api = retrofit.create(MeetupsApi.class);
+
+    public static MeetupsApi getApi(){
+        return api;
     }
+
 }
--- app/src/main/java/com/dusanjovanov/meetups3/rest/MeetupsAPI.java
+++ app/src/main/java/com/dusanjovanov/meetups3/rest/MeetupsAPI.java
@@ -5,16 +5,24 @@ import com.dusanjovanov.meetups3.models.User;
 import java.util.ArrayList;
 
 import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
 import retrofit2.http.Path;
 
 /**
  * Created by duca on 29/12/2016.
  */
 
-public interface MeetupsAPI  {
+public interface MeetupsApi {
     @GET("users/search/{query}")
     Call<ArrayList<User>> searchUsers(@Path("query") String query);
 
-
+    @FormUrlEncoded
+    @POST("users")
+    Call<Void> createUser(@Field("display_name") String displayName,
+                              @Field("email") String email,
+                              @Field("photo_url") String photoUrl,
+                              @Field("token") String token);
 }
