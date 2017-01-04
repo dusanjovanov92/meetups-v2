fe774524591666a1cebd7e2a49482f8fe26889b3
duca <jovanovdusan1@gmail.com>
2017-01-04 20:28:18 +0100

    prihvatanje prosledjenog korisnika i prosledjivanje fragmentu

--- app/src/main/java/com/dusanjovanov/meetups3/MainScreenActivity.java
+++ app/src/main/java/com/dusanjovanov/meetups3/MainScreenActivity.java
@@ -18,6 +18,7 @@ import android.view.View;
 import com.dusanjovanov.meetups3.fragments.ContactsFragment;
 import com.dusanjovanov.meetups3.fragments.GroupsFragment;
 import com.dusanjovanov.meetups3.fragments.HomeFragment;
+import com.dusanjovanov.meetups3.models.User;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class MainScreenActivity extends AppCompatActivity {
@@ -28,11 +29,14 @@ public class MainScreenActivity extends AppCompatActivity {
     private TabLayout tabs;
     private ViewPager pager;
     private PagerAdapter pagerAdapter;
+    private User currentUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_screen);
+
+        handleIntent();
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         toolbar.requestFocus();
@@ -40,9 +44,7 @@ public class MainScreenActivity extends AppCompatActivity {
         ActionBar actionBar = getSupportActionBar();
         if(actionBar!=null){
             actionBar.setDisplayShowTitleEnabled(false);
-//            actionBar.setIcon(R.drawable.ic_launcher);
         }
-
 
         pager = (ViewPager) findViewById(R.id.pager);
         pagerAdapter = new PagerAdapter(getSupportFragmentManager());
@@ -66,9 +68,22 @@ public class MainScreenActivity extends AppCompatActivity {
 
     }
 
+    private void handleIntent(){
+        Intent intent = getIntent();
+        String action = null;
+        if(intent!=null){
+            action = intent.getStringExtra("action");
+        }
+        if(action!=null){
+            if(action.equals(MainActivity.TAG)){
+                currentUser = (User) intent.getSerializableExtra("user");
+            }
+        }
+    }
+
     private void setTabIcons(){
         tabs.getTabAt(0).setIcon(R.drawable.tab_home);
-        tabs.getTabAt(1).setIcon(R.drawable.tab_home);
+        tabs.getTabAt(1).setIcon(R.drawable.tab_groups);
         tabs.getTabAt(2).setIcon(R.drawable.tab_home);
     }
 
@@ -84,7 +99,11 @@ public class MainScreenActivity extends AppCompatActivity {
                 case 0:
                     return new HomeFragment();
                 case 1:
-                    return new GroupsFragment();
+                    GroupsFragment groupsFragment = new GroupsFragment();
+                    Bundle args = new Bundle();
+                    args.putSerializable("user",currentUser);
+                    groupsFragment.setArguments(args);
+                    return groupsFragment;
                 case 2:
                     return new ContactsFragment();
                 default:
@@ -109,16 +128,6 @@ public class MainScreenActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_main_screen,menu);
-
-//        searchView = (SearchView) menu.findItem(R.id.action_search).getActionView();
-//        searchView.setQueryHint("Search for users...");
-//
-//        searchView.setOnSearchClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                startActivity(new Intent(MainScreenActivity.this,SearchActivity.class));
-//            }
-//        });
 
         return super.onCreateOptionsMenu(menu);
     }
