33ca2c0f6fbd920e3879b98057bbd6ca6b182346
duca <jovanovdusan1@gmail.com>
2017-01-04 20:27:45 +0100

    dohvata korisnika sa servera i prosledjuje dalje

--- app/src/main/java/com/dusanjovanov/meetups3/MainActivity.java
+++ app/src/main/java/com/dusanjovanov/meetups3/MainActivity.java
@@ -1,18 +1,25 @@
 package com.dusanjovanov.meetups3;
 
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 
+import com.dusanjovanov.meetups3.models.User;
+import com.dusanjovanov.meetups3.rest.ApiClient;
 import com.dusanjovanov.meetups3.util.FirebaseUtil;
 import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.firebase.auth.FirebaseUser;
 
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class MainActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener {
 
+    public static final String TAG = "TagMainActivity";
     private GoogleApiClient googleApiClient;
     private FirebaseUser currentUser;
 
@@ -33,8 +40,28 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.O
             finish();
         }
         else{
-            startActivity(new Intent(this,MainScreenActivity.class));
-            finish();
+            Call<User> call = ApiClient.getApi().getUser(currentUser.getEmail());
+            call.enqueue(new Callback<User>() {
+                @Override
+                public void onResponse(Call<User> call, Response<User> response) {
+                    if(response.isSuccessful()){
+                        Intent intent = new Intent(MainActivity.this,MainScreenActivity.class);
+                        intent.putExtra("action",TAG);
+                        intent.putExtra("user",response.body());
+                        startActivity(intent);
+                        finish();
+                    }
+                    else{
+
+                    }
+                }
+
+                @Override
+                public void onFailure(Call<User> call, Throwable t) {
+
+                }
+            });
+
         }
     }
 
