f7cbc105ca7b078cb44dae78b6a1b1a72d15d2f9
duca <jovanovdusan1@gmail.com>
2017-01-04 20:25:26 +0100

    uradio prikaz grupa u fragmentu sa cardview

--- app/src/main/java/com/dusanjovanov/meetups3/fragments/GroupsFragment.java
+++ app/src/main/java/com/dusanjovanov/meetups3/fragments/GroupsFragment.java
@@ -1,25 +1,104 @@
 package com.dusanjovanov.meetups3.fragments;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.dusanjovanov.meetups3.R;
+import com.dusanjovanov.meetups3.adapters.GroupsRecyclerAdapter;
+import com.dusanjovanov.meetups3.models.Group;
+import com.dusanjovanov.meetups3.models.User;
+import com.dusanjovanov.meetups3.rest.ApiClient;
+
+import java.util.ArrayList;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 /**
  * Created by duca on 30/12/2016.
  */
 
 public class GroupsFragment extends Fragment{
+
+    public static final String TAG = "TagGroupsFragment";
+    private RecyclerView rvGroups;
+    private TextView txtNoGroups;
+    private GroupsRecyclerAdapter adapter;
+    private ArrayList<Group> groups = new ArrayList<>();
+    private Context context;
+    private User currentUser;
+
     public GroupsFragment() {
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        this.context = context;
+        adapter = new GroupsRecyclerAdapter(groups,context);
+        Bundle args = getArguments();
+        if(args!=null){
+            currentUser = (User) args.getSerializable("user");
+        }
     }
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_groups,container,false);
+        View fragment = inflater.inflate(R.layout.fragment_groups,container,false);
+        rvGroups = (RecyclerView) fragment.findViewById(R.id.rv_groups);
+        rvGroups.setAdapter(adapter);
+        GridLayoutManager layoutManager = new GridLayoutManager(context,2);
+        rvGroups.setLayoutManager(layoutManager);
+        GroupsRecyclerAdapter.SpacesItemDecoration itemDecoration = new GroupsRecyclerAdapter.SpacesItemDecoration(10);
+        rvGroups.addItemDecoration(itemDecoration);
+        txtNoGroups = (TextView) fragment.findViewById(R.id.txt_no_groups);
+        return fragment;
+    }
+
+    @Override
+    public void setUserVisibleHint(boolean isVisibleToUser) {
+        if(isVisibleToUser){
+            getGroups();
+        }
+    }
+
+    private void getGroups(){
+        Call<ArrayList<Group>> call = ApiClient.getApi().getGroups(currentUser.getId());
+        call.enqueue(new Callback<ArrayList<Group>>() {
+            @Override
+            public void onResponse(Call<ArrayList<Group>> call, Response<ArrayList<Group>> response) {
+                if(response.isSuccessful()){
+                    Log.v(TAG,response.raw().toString());
+                    groups.clear();
+                    if(response.body().size()<1){
+                        txtNoGroups.setVisibility(View.VISIBLE);
+                    }
+                    else{
+                        txtNoGroups.setVisibility(View.GONE);
+                        groups.addAll(response.body());
+                        adapter.notifyDataSetChanged();
+                    }
+                }
+                else{
+
+                }
+            }
+
+            @Override
+            public void onFailure(Call<ArrayList<Group>> call, Throwable t) {
+
+            }
+        });
     }
 }
