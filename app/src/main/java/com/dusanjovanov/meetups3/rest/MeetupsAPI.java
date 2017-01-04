83e6bea8dbe687c172541d1c8c86f1d7fa8fa088
duca <jovanovdusan1@gmail.com>
2017-01-04 20:26:56 +0100

    dodao nove pozive

--- app/src/main/java/com/dusanjovanov/meetups3/rest/MeetupsAPI.java
+++ app/src/main/java/com/dusanjovanov/meetups3/rest/MeetupsAPI.java
@@ -1,5 +1,6 @@
 package com.dusanjovanov.meetups3.rest;
 
+import com.dusanjovanov.meetups3.models.Group;
 import com.dusanjovanov.meetups3.models.User;
 
 import java.util.ArrayList;
@@ -16,13 +17,19 @@ import retrofit2.http.Path;
  */
 
 public interface MeetupsApi {
+    @GET("users/getByEmail/{email}")
+    Call<User> getUser(@Path("email")String email);
+
     @GET("users/search/{query}")
     Call<ArrayList<User>> searchUsers(@Path("query") String query);
 
     @FormUrlEncoded
     @POST("users")
-    Call<Void> createUser(@Field("display_name") String displayName,
+    Call<User> createUser(@Field("display_name") String displayName,
                               @Field("email") String email,
                               @Field("photo_url") String photoUrl,
                               @Field("token") String token);
+
+    @GET("users/{id_user}/groups")
+    Call<ArrayList<Group>> getGroups(@Path("id_user") int idUser);
 }
